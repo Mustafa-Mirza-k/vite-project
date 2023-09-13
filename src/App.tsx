50d49f673d8e60d1942e8c8d31c827ca6940c9c7
Mustafa mirza <mustafa.mirza@ignitelabs.co>
2023-09-13 11:02:50 +0500

    layout set

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import "./App.css";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-     <Button colorScheme='blue'>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"orange.300"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"green.300"}>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area={"main"} bg={"blue.300"}>
+          Main
+        </GridItem>
+      </Grid>
+      {/* <Button colorScheme="blue">Button</Button> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
